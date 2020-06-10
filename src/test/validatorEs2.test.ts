237da9e2ca5207c3531cd68bb1c4baff3a8858cc
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-09 17:09:59 -0700

    renaming

--- src/test/validatorEs2.test.ts
+++ src/test/validatorEs2.test.ts
@@ -49,8 +49,8 @@ t.test('authorCanWriteToKey', (t: any) => {
     t.done();
 });
 
-t.test('hashItem', (t: any) => {
-    let item1: Document = {
+t.test('hashDocument', (t: any) => {
+    let doc1: Document = {
         format: 'es.2',
         workspace: 'gardenclub',
         path: 'k1',
@@ -59,12 +59,12 @@ t.test('hashItem', (t: any) => {
         author: '@me.ed25519',
         signature: 'xxx.sig.ed25519',
     };
-    t.equal(Val.hashDocument(item1), '89b37cdfb52ac4c6d85599cdb19d1ff0d6340b924478ea7db36cce3912839db3');
+    t.equal(Val.hashDocument(doc1), '89b37cdfb52ac4c6d85599cdb19d1ff0d6340b924478ea7db36cce3912839db3');
     t.done();
 });
 
-t.test('signItem and itemSignatureIsValid', (t: any) => {
-    let item1: Document = {
+t.test('signDocument and documentSignatureIsValid', (t: any) => {
+    let doc1: Document = {
         format: 'es.2',
         workspace: 'gardenclub',
         path: 'k1',
@@ -73,81 +73,43 @@ t.test('signItem and itemSignatureIsValid', (t: any) => {
         author: author1,
         signature: '',
     };
-    t.notOk(Val.documentSignatureIsValid(item1), 'item with empty sig is not valid');
+    t.notOk(Val.documentSignatureIsValid(doc1), 'doc with empty sig is not valid');
 
-    let signedItem = Val.signDocument(keypair1, item1);
-    t.ok(signedItem.signature.length > 10, 'item looks like it has some kind of signature');
-    t.ok(Val.documentSignatureIsValid(signedItem), 'signature is actually valid');
+    let signedDoc = Val.signDocument(keypair1, doc1);
+    t.ok(signedDoc.signature.length > 10, 'doc looks like it has some kind of signature');
+    t.ok(Val.documentSignatureIsValid(signedDoc), 'signature is actually valid');
 
     // modify various things and ensure the signature becomes invalid
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, signature: 'xxx.sig.ed25519'}),
+        Val.documentSignatureIsValid({...signedDoc, signature: 'xxx.sig.ed25519'}),
         'garbage sig is not valid'
     );
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, format: 'xxx' as any}),
+        Val.documentSignatureIsValid({...signedDoc, format: 'xxx' as any}),
         'sig not valid if schema changes'
     );
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, path: 'xxx'}),
+        Val.documentSignatureIsValid({...signedDoc, path: 'xxx'}),
         'sig not valid if key changes'
     );
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, value: 'xxx'}),
+        Val.documentSignatureIsValid({...signedDoc, value: 'xxx'}),
         'sig not valid if value changes'
     );
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, timestamp: 9999}),
+        Val.documentSignatureIsValid({...signedDoc, timestamp: 9999}),
         'sig not valid if timestamp changes'
     );
     t.notOk(
-        Val.documentSignatureIsValid({...signedItem, author: '@notme.ed25519'}),
+        Val.documentSignatureIsValid({...signedDoc, author: '@notme.ed25519'}),
         'sig not valid if author changes'
     );
 
     t.done();
 });
 
-/*
-// this was moved to storeMemory.ts
-t.test('historySortFn', (t: any) => {
-    let item1: Item = {
-        format: 'es.2',
-        workspace: 'gardenclub',
-        key: 'k1',
-        value: 'v1',
-        timestamp: 1,
-        author: '@1.ed25519',
-        signature: 'xxx',
-    };
-    let item2a: Item = {
-        format: 'es.2',
-        workspace: 'gardenclub',
-        key: 'k2',
-        value: 'v2',
-        timestamp: 2,
-        author: '@2.ed25519',
-        signature: 'aaa',
-    };
-    let item2b: Item = {
-        format: 'es.2',
-        workspace: 'gardenclub',
-        key: 'k2',
-        value: 'v2',
-        timestamp: 2,
-        author: '@2.ed25519',
-        signature: 'bbb',
-    };
-    let input = [item1, item2a, item2b];
-    let correct = [item2b, item2a, item1];  // timestamp DESC, signatures DESC
-    input.sort(historySortFn);
-    t.same(input, correct, 'historySortFn: sort order is correct');
-    t.done();
-});
-*/
-
-t.test('itemIsValid', (t: any) => {
-    let item1: Document = {
+t.test('documentIsValid', (t: any) => {
+    let doc1: Document = {
         format: 'es.2',
         workspace: 'gardenclub',
         path: 'k1',
@@ -156,46 +118,46 @@ t.test('itemIsValid', (t: any) => {
         author: author1,
         signature: 'xxx',
     };
-    let signedItem = Val.signDocument(keypair1, item1);
+    let signedDoc = Val.signDocument(keypair1, doc1);
 
-    t.ok(Val.documentIsValid(signedItem), 'valid item');
+    t.ok(Val.documentIsValid(signedDoc), 'valid doc');
 
-    t.notOk(Val.documentIsValid({...signedItem, format: false as any}), 'format wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, workspace: false as any}), 'workspace wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, path: false as any}), 'key wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, value: false as any}), 'value wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, timestamp: false as any}), 'timestamp wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, author: false as any}), 'author wrong datatype');
-    t.notOk(Val.documentIsValid({...signedItem, signature: false as any}), 'signature wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, format: false as any}), 'format wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, workspace: false as any}), 'workspace wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, path: false as any}), 'key wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, value: false as any}), 'value wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, timestamp: false as any}), 'timestamp wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, author: false as any}), 'author wrong datatype');
+    t.notOk(Val.documentIsValid({...signedDoc, signature: false as any}), 'signature wrong datatype');
 
-    t.notOk(Val.documentIsValid({...signedItem, extra: 'xxx'} as any), 'extra property in object');
+    t.notOk(Val.documentIsValid({...signedDoc, extra: 'xxx'} as any), 'extra property in object');
 
-    t.notOk(Val.documentIsValid({...signedItem, format: snowmanJsString}), 'format non-ascii');
-    t.notOk(Val.documentIsValid({...signedItem, workspace: snowmanJsString}), 'workspace non-ascii');
-    t.notOk(Val.documentIsValid({...signedItem, path: snowmanJsString}), 'key non-ascii');
-    t.notOk(Val.documentIsValid({...signedItem, author: snowmanJsString}), 'author non-ascii');
-    t.notOk(Val.documentIsValid({...signedItem, signature: snowmanJsString}), 'signature non-ascii');
+    t.notOk(Val.documentIsValid({...signedDoc, format: snowmanJsString}), 'format non-ascii');
+    t.notOk(Val.documentIsValid({...signedDoc, workspace: snowmanJsString}), 'workspace non-ascii');
+    t.notOk(Val.documentIsValid({...signedDoc, path: snowmanJsString}), 'key non-ascii');
+    t.notOk(Val.documentIsValid({...signedDoc, author: snowmanJsString}), 'author non-ascii');
+    t.notOk(Val.documentIsValid({...signedDoc, signature: snowmanJsString}), 'signature non-ascii');
 
-    t.notOk(Val.documentIsValid({...signedItem, format: '\n'}), 'newline in format');
-    t.notOk(Val.documentIsValid({...signedItem, workspace: '\n'}), 'newline in workspace');
-    t.notOk(Val.documentIsValid({...signedItem, path: '\n'}), 'newline in key');
-    t.notOk(Val.documentIsValid({...signedItem, author: '\n'}), 'newline in author');
-    t.notOk(Val.documentIsValid({...signedItem, signature: '\n'}), 'newline in signature');
+    t.notOk(Val.documentIsValid({...signedDoc, format: '\n'}), 'newline in format');
+    t.notOk(Val.documentIsValid({...signedDoc, workspace: '\n'}), 'newline in workspace');
+    t.notOk(Val.documentIsValid({...signedDoc, path: '\n'}), 'newline in key');
+    t.notOk(Val.documentIsValid({...signedDoc, author: '\n'}), 'newline in author');
+    t.notOk(Val.documentIsValid({...signedDoc, signature: '\n'}), 'newline in signature');
 
-    t.notOk(Val.documentIsValid({...signedItem, format: 'xxxxxx' as any}), 'unknown format');
+    t.notOk(Val.documentIsValid({...signedDoc, format: 'xxxxxx' as any}), 'unknown format');
 
-    let missingKey = {...signedItem};
+    let missingKey = {...signedDoc};
     delete missingKey.path;
     t.notOk(Val.documentIsValid(missingKey), 'missing key');
 
-    t.notOk(Val.documentIsValid({...signedItem, author: 'a\nb'}), 'newline in author');
-    t.notOk(Val.documentIsValid({...signedItem, path: '\n'}), 'invalid key');
-    t.notOk(Val.documentIsValid({...signedItem, path: '{}'}), 'no write permission');
+    t.notOk(Val.documentIsValid({...signedDoc, author: 'a\nb'}), 'newline in author');
+    t.notOk(Val.documentIsValid({...signedDoc, path: '\n'}), 'invalid key');
+    t.notOk(Val.documentIsValid({...signedDoc, path: '{}'}), 'no write permission');
 
-    t.notOk(Val.documentIsValid(item1), 'bad signature');
-    t.notOk(Val.documentIsValid({...signedItem, timestamp: now / 1000}), 'timestamp too small, probably in milliseconds');
-    t.notOk(Val.documentIsValid({...signedItem, timestamp: now * 2}), 'timestamp in future');
-    t.notOk(Val.documentIsValid({...signedItem, timestamp: Number.MAX_SAFE_INTEGER * 2}), 'timestamp way too large');
+    t.notOk(Val.documentIsValid(doc1), 'bad signature');
+    t.notOk(Val.documentIsValid({...signedDoc, timestamp: now / 1000}), 'timestamp too small, probably in milliseconds');
+    t.notOk(Val.documentIsValid({...signedDoc, timestamp: now * 2}), 'timestamp in future');
+    t.notOk(Val.documentIsValid({...signedDoc, timestamp: Number.MAX_SAFE_INTEGER * 2}), 'timestamp way too large');
 
     t.done();
 });
