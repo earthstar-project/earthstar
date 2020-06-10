1ae8d54d05110bc50f574b84e286d6d99d0ed496
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-09 17:05:01 -0700

    renaming

--- src/storage/memory.ts
+++ src/storage/memory.ts
@@ -18,8 +18,8 @@ let log = (...args : any[]) => void {};  // turn off logging for now
 let logWarning = (...args : any[]) => void {};  // turn off logging for now
 
 export let _historySortFn = (a: Document, b: Document): number => {
-    // Sorts items within one key from multiple authors,
-    // so that the winning item is first.
+    // Sorts docs within one key from multiple authors,
+    // so that the winning doc is first.
     // timestamp DESC (newest first), signature DESC (to break timestamp ties)
     if (a.timestamp < b.timestamp) {
         return 1;
@@ -39,24 +39,24 @@ export let _historySortFn = (a: Document, b: Document): number => {
 export class StorageMemory implements IStorage {
     /*
     This uses an in-memory data structure:
-    _items:
+    _docs:
     {
         keyA: {
-            @author1: {...ITEM...},
-            @author2: {...ITEM...},
+            @author1: {...DOC...},
+            @author2: {...DOC...},
         }
         keyB: {
-            @author1: {...ITEM...},
+            @author1: {...DOC...},
         }
     }
-    _items[key] is never an empty object, it's always missing or contains items.
+    _docs[key] is never an empty object, it's always missing or contains docs.
 
-    Each key can have one item per author.
-    Keys with write permissions will only have one author, thus only one item.
+    Each key can have one doc per author.
+    Keys with write permissions will only have one author, thus only one doc.
     Public keys can have multiple authors, but one is considered the winner
       (with the highest timestamp).
     */
-    _items : {[key:string] : {[author:string] : Document}} = {};
+    _docs : {[key:string] : {[author:string] : Document}} = {};
     workspace : WorkspaceAddress;
     validatorMap : {[format: string] : IValidator};
     onChange : Emitter<undefined>;
@@ -80,14 +80,14 @@ export class StorageMemory implements IStorage {
 
         // if asking for a single key, check if it exists and return it by itself
         if (query.path !== undefined) {
-            if (this._items[query.path] !== undefined) {
+            if (this._docs[query.path] !== undefined) {
                 return [query.path];
             } else {
                 return [];
             }
         }
 
-        let keys = Object.keys(this._items);
+        let keys = Object.keys(this._docs);
         keys.sort();
 
         // filter the keys in various ways
@@ -105,43 +105,43 @@ export class StorageMemory implements IStorage {
         return keys;
     }
     documents(query? : QueryOpts) : Document[] {
-        // return items that match the query, sorted by keys.
+        // return docs that match the query, sorted by keys.
         // TODO: note that opts.limit applies to the number of keys,
-        //   not the number of unique history items
+        //   not the number of unique history docs
 
-        //log('------------------------------------------ ITEMS');
+        //log('------------------------------------------ DOCS');
         //log('query', JSON.stringify(query));
         let includeHistory = query?.includeHistory === true;  // default to false
         let keys = this.paths(query);
         //log('keys', keys);
-        let items : Document[] = [];
+        let docs : Document[] = [];
         for (let key of keys) {
             //log('key', key);
-            let keyHistoryItems = Object.values(this._items[key]);
+            let keyHistoryDocs = Object.values(this._docs[key]);
             // sort by timestamp etc
-            //log(JSON.stringify(keyHistoryItems, null, 4));
+            //log(JSON.stringify(keyHistoryDocs, null, 4));
             //log('sorting newest first...');
-            keyHistoryItems.sort(_historySortFn);
-            //log(JSON.stringify(keyHistoryItems, null, 4));
+            keyHistoryDocs.sort(_historySortFn);
+            //log(JSON.stringify(keyHistoryDocs, null, 4));
             if (includeHistory) {
-                items = items.concat(keyHistoryItems);
+                docs = docs.concat(keyHistoryDocs);
             } else {
-                items.push(keyHistoryItems[0]);
+                docs.push(keyHistoryDocs[0]);
             }
         }
-        return items;
+        return docs;
     }
     values(query? : QueryOpts) : string[] {
-        // get items that match the query, sort by key, and return their values.
+        // get docs that match the query, sort by key, and return their values.
         // TODO: note that opts.limit applies to the number of keys,
-        //   not the number of unique history items
-        return this.documents(query).map(item => item.value);
+        //   not the number of unique history docs
+        return this.documents(query).map(doc => doc.value);
     }
 
     authors() : RawCryptKey[] {
         let authorSet : Set<RawCryptKey> = new Set();
-        for (let item of this.documents({ includeHistory: true })) {
-            authorSet.add(item.author);
+        for (let doc of this.documents({ includeHistory: true })) {
+            authorSet.add(doc.author);
         }
         let authors = [...authorSet];
         authors.sort();
@@ -151,108 +151,108 @@ export class StorageMemory implements IStorage {
     getDocument(key : string) : Document | undefined {
         // look up the winning value for a single key.
         // return undefined if not found.
-        // to get history items for a key, do items({key: 'foo', includeHistory: true})
-        if (this._items[key] === undefined) { return undefined; }
-        let keyHistoryItems = Object.values(this._items[key]);
-        keyHistoryItems.sort(_historySortFn);
-        return keyHistoryItems[0];
+        // to get history docs for a key, do documents({key: 'foo', includeHistory: true})
+        if (this._docs[key] === undefined) { return undefined; }
+        let keyHistoryDocs = Object.values(this._docs[key]);
+        keyHistoryDocs.sort(_historySortFn);
+        return keyHistoryDocs[0];
     }
     getValue(key : string) : string | undefined {
-        // same as getItem, but just returns the value, not the whole item object.
+        // same as getDocument, but just returns the value, not the whole doc object.
         return this.getDocument(key)?.value;
     }
 
-    ingestDocument(item : Document, futureCutoff? : number) : boolean {
-        // Given an item from elsewhere, validate, decide if we want it, and possibly store it.
+    ingestDocument(doc : Document, futureCutoff? : number) : boolean {
+        // Given an doc from elsewhere, validate, decide if we want it, and possibly store it.
         // Return true if we kept it, false if we rejected it.
 
         // It can be rejected if it's not the latest one from the same author,
-        // or if the item is invalid (signature, etc).
+        // or if the doc is invalid (signature, etc).
 
-        // Within a single key we keep the one latest item from each author.
-        // So this overwrites older items from the same author - they are forgotten.
+        // Within a single key we keep the one latest doc from each author.
+        // So this overwrites older docs from the same author - they are forgotten.
         // If it's from a new author for this key, we keep it no matter the timestamp.
-        // The winning item is chosen at get time, not write time.
+        // The winning doc is chosen at get time, not write time.
 
         // futureCutoff is a timestamp in microseconds.
         // Messages from after that are ignored.
         // Defaults to now + 10 minutes.
         // This prevents malicious peers from sending very high timestamps.
 
-        let validator = this.validatorMap[item.format];
+        let validator = this.validatorMap[doc.format];
         if (validator === undefined) {
-            logWarning(`ingestItem: unrecognized format ${item.format}`);
+            logWarning(`ingestDocument: unrecognized format ${doc.format}`);
             return false;
         }
 
-        if (!validator.documentIsValid(item, futureCutoff)) {
-            logWarning(`ingestItem: item is not valid`);
+        if (!validator.documentIsValid(doc, futureCutoff)) {
+            logWarning(`ingestDocument: doc is not valid`);
             return false;
         }
 
-        // Only accept items from the same workspace.
-        if (item.workspace !== this.workspace) {
-            logWarning(`ingestItem: item from different workspace`);
+        // Only accept docs from the same workspace.
+        if (doc.workspace !== this.workspace) {
+            logWarning(`ingestDocument: doc from different workspace`);
             return false;
         }
 
-        let existingItemsByKey = this._items[item.path] || {};
-        let existingFromSameAuthor = existingItemsByKey[item.author];
+        let existingDocsByKey = this._docs[doc.path] || {};
+        let existingFromSameAuthor = existingDocsByKey[doc.author];
 
         // Compare timestamps.
         // Compare signature to break timestamp ties.
         if (existingFromSameAuthor !== undefined
-            && [item.timestamp, item.signature]
+            && [doc.timestamp, doc.signature]
             <= [existingFromSameAuthor.timestamp, existingFromSameAuthor.signature]
             ) {
-            // incoming item is older or identical.  ignore it.
-            logWarning(`ingestItem: item older or identical`);
+            // incoming doc is older or identical.  ignore it.
+            logWarning(`ingestDoc: doc older or identical`);
             return false;
         }
 
-        existingItemsByKey[item.author] = item;
-        this._items[item.path] = existingItemsByKey;
+        existingDocsByKey[doc.author] = doc;
+        this._docs[doc.path] = existingDocsByKey;
         this.onChange.send(undefined);
         return true;
     }
 
-    set(keypair : Keypair, itemToSet : DocToSet) : boolean {
+    set(keypair : Keypair, docToSet : DocToSet) : boolean {
         // Store a value.
         // Timestamp is optional and should normally be omitted or set to 0,
         // in which case it will be set to now().
         // (New writes should always have a timestamp of now() except during
         // unit testing or if you're importing old data.)
 
-        let validator = this.validatorMap[itemToSet.format];
+        let validator = this.validatorMap[docToSet.format];
         if (validator === undefined) {
-            logWarning(`set: unrecognized format ${itemToSet.format}`);
+            logWarning(`set: unrecognized format ${docToSet.format}`);
             return false;
         }
 
-        itemToSet.timestamp = itemToSet.timestamp || 0;
-        let item : Document = {
-            format: itemToSet.format,
+        docToSet.timestamp = docToSet.timestamp || 0;
+        let doc : Document = {
+            format: docToSet.format,
             workspace: this.workspace,
-            path: itemToSet.path,
-            value: itemToSet.value,
+            path: docToSet.path,
+            value: docToSet.value,
             author: keypair.public,
-            timestamp: itemToSet.timestamp > 0 ? itemToSet.timestamp : Date.now()*1000,
+            timestamp: docToSet.timestamp > 0 ? docToSet.timestamp : Date.now()*1000,
             signature: '',
         }
 
-        // If there's an existing item from anyone,
+        // If there's an existing doc from anyone,
         // make sure our timestamp is greater
         // even if this puts us slightly into the future.
-        // (We know about the existing item so let's assume we want to supercede it.)
-        let existingItemTimestamp = this.getDocument(item.path)?.timestamp || 0;
-        item.timestamp = Math.max(item.timestamp, existingItemTimestamp+1);
+        // (We know about the existing doc so let's assume we want to supercede it.)
+        let existingDocTimestamp = this.getDocument(doc.path)?.timestamp || 0;
+        doc.timestamp = Math.max(doc.timestamp, existingDocTimestamp+1);
 
-        let signedItem = validator.signDocument(keypair, item);
-        return this.ingestDocument(signedItem, item.timestamp);
+        let signedDoc = validator.signDocument(keypair, doc);
+        return this.ingestDocument(signedDoc, doc.timestamp);
     }
 
     _syncFrom(otherStore : IStorage, existing : boolean, live : boolean) : number {
-        // Pull all items from the other Store and ingest them one by one.
+        // Pull all docs from the other Store and ingest them one by one.
 
         let numSuccess = 0;
         if (live) {
@@ -260,8 +260,8 @@ export class StorageMemory implements IStorage {
             throw "live sync not implemented yet";
         }
         if (existing) {
-            for (let item of otherStore.documents({includeHistory: true})) {
-                let success = this.ingestDocument(item);
+            for (let doc of otherStore.documents({includeHistory: true})) {
+                let success = this.ingestDocument(doc);
                 if (success) { numSuccess += 1; }
             }
         }
@@ -273,7 +273,7 @@ export class StorageMemory implements IStorage {
         //   opts.direction: 'push', 'pull', or 'both'
         //   opts.existing: Sync existing values.  Default true.
         //   opts.live (not implemented yet): Continue streaming new changes forever
-        // Return the number of items pushed and pulled.
+        // Return the number of docs pushed and pulled.
         // This uses a simple and inefficient algorithm.  Fancier algorithm TBD.
 
         // don't sync with yourself
