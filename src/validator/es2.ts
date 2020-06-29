2186ceaf81303985ac80e6641d5bdf332ebc2921
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-06-29 16:01:19 -0700

    disallow consecutive slashes in paths

--- src/validator/es2.ts
+++ src/validator/es2.ts
@@ -29,6 +29,10 @@ export const ValidatorEs2 : IValidator = class {
             logWarning('invalid path: does not start with /');
             return false;
         }
+        if (path.indexOf('//') !== -1) {
+            logWarning('invalid path: contains two consecutive slashes');
+            return false;
+        }
         if (!onlyHasChars(path, pathChars)) {
             logWarning('invalid path: contains disallowed characters');
             return false;
