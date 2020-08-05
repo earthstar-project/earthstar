edc4bc3e930ae123e07845db461cb5d62377d617
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-08-04 19:05:38 -0700

    clean up errorstyles.ts example file

--- ignore/errorstyles.ts
+++ ignore/errorstyles.ts
@@ -1,4 +1,7 @@
 
+//================================================================================
+// SETUP
+
 type Dog = {
     name : string,
     age : number,
@@ -21,165 +24,73 @@ let isSuccess = <T>(result : Result<T>) : result is T =>
 let log = console.log;
 
 
-/*
-                no null ref                     overall
-                        easy check for err
-                                err check does type guarding
-                                        can get err message
+//================================================================================
+// DIFFERENT STYLES OF HANDLING ERRORS
 
-Dog|Err         good    fn      good    good    weird err check, simple types, shadowing of 'err' field
+// Custom error style
+// Our Err type is {err: string}
+let makeDogA = (name : string) : Dog | Err => {
+    if (name === '') { return {err: 'nope'}; }
+    return { name: name, age: 5 };
+}
+let dogA = makeDogA('');
+log(dogA.age);  // good, error as expected
+if ('err' in dogA) {
+    log(dogA.err);  // good
+} else {
+    log(dogA.age);  // good
+}
 
-Dog|Error       shadow  fn      good    good    weird err check, simple types, shadowing of fields from Error (type, message, stack)  this is Result<T>
-
-[Err | null,    good    good    oneway  good    ok if used correctly, not as wordy
- Dog | null]
-
----------------
-
-{dog?, err?}    good    good    oneway  good    ok if used correctly, wordy
-
-{dog | null,    good    good    oneway  good    ok if used correctly, wordy
- err | null}
-
-throw           no err  long    good    good    err is not captured in type info (easy to forget).  easy to chain though.
-
-Dog|null        good    good    good    no      nice and simple but no error info
-
-*/
-
-
-
-
-
-
-
-// https://spin.atomicobject.com/2018/02/23/error-either-typescript/
-
-let makeDogResult = (name : string) : Dog | Error => {   // or : Result<Dog>
+// Result<T> style
+// Result<T> is the same as T | Error
+// This uses the built-in node Error type.
+let makeDogB = (name : string) : Dog | Error => {
     if (name === '') { return new Error('nope'); }
     return { name: name, age: 5 };
 }
-let dog0 = makeDogResult('');
-log(dog0.name);  // :( Error fields "name", "message", and "stack" collide with Dog fields
-log(dog0.age);  // good, error as expected
-if (dog0 instanceof Error) {
-    log(dog0.message);  // good
+let dogB = makeDogB('');
+log(dogB.name);  // :( Error fields "name", "message", and "stack" collide with Dog fields
+                 //    allowing an accidentally successful line of code here, which should be an error
+log(dogB.age);  // at least this one is an error, good
+if (dogB instanceof Error) {
+    log(dogB.message);  // good
 } else {
-    log(dog0.age);  // good
-}
-if (!isError(dog0)) {
-    log(dog0.name);  // good
-}
-if (isSuccess(dog0)) {
-    log(dog0.name);  // good
-} else {
-    log(dog0.message);  // good, can get error message
+    log(dogB.age);  // good
 }
 
-
-let makeDogOrNull = (name : string) : Dog | null => {
-    if (name === '') { return null; }
-    return { name: name, age: 5 };
-}
-let dog1 = makeDogOrNull('');
-log(dog1.name);  // good, error as expected
-if (dog1 !== null) {
-    log(dog1.name);  // good
-} else {
-    // :( no way to get a string for the error about what went wrong
-}
-
-
-let makeDogOrThrow = (name : string) : Dog => {
-    if (name === '') { throw new Error('nope'); }
-    return { name: name, age: 5 };
-}
-let dog2 = makeDogOrThrow('');
-log(dog2.name);  // :( no compile error, you have to remember to catch errors yourself
-try {
-    let dog2b = makeDogOrThrow('');
-    log(dog2b.name);
-} catch (err2) {
-    log(err2);  // good, can get error message
-}
-
-
-let makeDogOrErr = (name : string) : Dog | Err => {
-    if (name === '') { return { err: "nope" }; }
-    return { name: name, age: 5 };
-}
-let dog3 = makeDogOrErr('');
-log(dog3.name);  // good, error as expected
-if ('err' in dog3) {  // weird
-    log(dog3.err);  // good, can get error message
-} else {
-    log(dog3.name);  // good
-    log(dog3.age);
-}
-if (notErr(dog3)) {
-    log(dog3.age);  // good
-}
-if (isErr(dog3)) {
-    log(dog3.err);  // good
-}
-
-
-let makeDogOrErrObj = (name : string) : {dog? : Dog, err?: string} => {
-    if (name === '') { return { err: "nope" }; }
-    return { dog: { name: name, age: 5 } };
-}
-let {dog:dog4, err:err4} = makeDogOrErrObj('');
-log(dog4.name);  // good, error as expected
-if (dog4) {
-    log(dog4.name);  // good
-} else {
-    log(err4);  // good, can get error message though it's not enforced to be defined
-}
-if (!err4) {
-    log(dog4.name);  // :( don't check err, have to check dog
-} else {
-    log(err4);  // good, can get error message
-}
-
-
-let makeDogOrErrObjNull = (name : string) : {dog : Dog | null, err: string | null} => {
-    if (name === '') { return { dog: null, err: "nope" }; }
-    return { dog: { name: name, age: 5 }, err: null };
-}
-let {dog:dog5, err:err5} = makeDogOrErrObjNull('');
-log(dog5.name);  // good, error as expected
-if (dog5) {
-    log(dog5.name);  // good
-} else {
-    log(err5);  // good, can get error message though it's not enforced to be defined
-}
-if (!err5) {
-    log(dog5.name);  // :( don't check err, have to check dog
-} else {
-    log(err5);  // good, can get error message
-}
-
-
-let makeDogNodeStyle = (name : string) : [string | null, Dog | null] => {
+// node style
+// Return either ["error details", null] or [null, successfulObject]
+//
+let makeDogC = (name : string) : [string | null, Dog | null] => {
     if (name === '') { return ["nope", null]; }
     return [null, { name: name, age: 5 }];
 }
-let [err6, dog6] = makeDogNodeStyle('');
-log(dog6.name);  // good, error as expected
-if (dog6) {
-    log(dog6.name);  // good
+let [errC, dogC] = makeDogC('');
+log(dogC.name);  // good, error as expected
+if (dogC) {
+    log(dogC.name);  // good
 } else {
-    log(err6);  // good, can get error message
+    log(errC);  // good, can get error message
 }
-if (!err6) {
-    log(dog6.name);  // :( don't check err, have to check dog
+if (!errC) {
+    log(dogC.name);  // checking !err is not enough to satisfy Typescript, have to check dog directly
 } else {
-    log(err6);  // good, can get error message
+    log(errC);  // good, can get error message
 }
 
-
-
-
-
-
-
+// Classic throw/catch
+// The fatal flaw of this one is Typescript doesn't track which exceptions
+// can be thrown by a function, so you don't know when to use try/catch.
+let makeDogD = (name : string) : Dog => {  // this looks safe to me...
+    if (name === '') { throw new Error('nope'); }
+    return { name: name, age: 5 };
+}
+let dogD = makeDogD('');
+log(dogD.name);  // :( no compile error, you have to remember this function can throw, and catch errors yourself
+// if you remember to do a try/catch:, it takes up a lot of space:
+try {
+    let dogD2 = makeDogD('');
+    log(dogD2.name);  // good, no type danger here
+} catch (err2) {
+    log(err2);  // good, can get error message
+}
