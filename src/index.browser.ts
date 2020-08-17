f3ba296f4481c6c8551f68363e6b88ae476c0929
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-08-17 16:14:29 -0700

    export more utils from index.ts

--- src/index.browser.ts
+++ src/index.browser.ts
@@ -1,12 +1,18 @@
+// Browserify uses this file instead of index.ts
+// It should be the same as index.ts but with sqlite removed.
+
 export * from './crypto/crypto';
 export * from './crypto/cryptoTypes';
 export * from './crypto/encoding';
 export * from './layers/about';
 export * from './layers/wiki';
 export * from './storage/memory';
-//export * from './storage/sqlite';
+//export * from './storage/sqlite';  // doesn't work in browsers
 export * from './sync';
+export * from './util/characters';
+export * from './util/detRandom';
 export * from './util/emitter';
+export * from './util/helpers';
 export * from './util/types';
 export * from './validator/es4';
 export * from './workspace';
