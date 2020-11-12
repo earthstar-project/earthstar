cfac2acbce8af0c8bdd1817b9eefc4c0977a68fe
Cinnamon <cinnamon_bun_github@fastmail.com>
2020-11-11 17:13:55 -0800

    Add comments about path characters

--- src/util/characters.ts
+++ src/util/characters.ts
@@ -34,12 +34,40 @@ export const authorAddressChars = authorShortnameChars + b32chars + '@.';
 export const workspaceNameChars = alphaLower + digits;
 export const workspaceAddressChars = workspaceNameChars + b32chars + '+.';
 
-// Path special characters should be usable in regular web URLs without percent-encoding.
-// Notably not allowed:
-//    space  not allowed in URLs
-//    "      not allowed in URLs
-//    ?      to avoid confusion with URL query parameters
-export const pathPunctuation = "/'()-._~!*$&+,:=@%";
+// Characters allowed in Earthstar paths
+//---------------------------------------------
+// These allowed path characters should be usable in regular web URLs
+// without percent-encoding them or interfering with the rest of the URL.
+//
+// Allowed      Earthstar Meaning
+//    /            path separator
+//    !            ephemeral docs must have '!'
+//    ~            path ownership (write permissions)
+//    +@.          used by workspace and author names but allowed elsewhere too
+//    %            used for percent-encoding
+//    '()-_*$&,:=  no meaning
+//    a-zA-Z0-9    no meaning
+//
+// Disallowed       Reason
+//    space            not allowed in URLs
+//    <>"[\]^`{|}      not allowed in URLs
+//    ?                to avoid confusion with URL query parameters
+//    #                to avoid confusion with URL anchors
+//    ;                no reason
+//    non-ASCII chars  to avoid trouble with Unicode normalization
+//    ASCII whitespace
+//    ASCII control characters
+//
+//
+// To use other characters in a path, percent-encode them using encodeURI.
+// For example
+//      desiredPath = '/food/🍆/nutrition'
+//      earthstarPath = encodeURI(desiredPath);  // --> '/food/%F0%9F%8D%86/nutrition'
+//      store it as earthstarPath
+//      for display to users again, run decodeURI(earthstarPath)
+//
+
+export const pathPunctuation = "/'()-._~!*$&+,:=@%";  // note double quotes are not included
 export const pathChars = alphaLower + alphaUpper + digits + pathPunctuation;
 
 
