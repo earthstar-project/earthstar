a16981e8b399a6a2ac38db7a578d99b890b87113
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-04-05 17:40:04 -0700

    clean up query

--- src/soup.ts
+++ src/soup.ts
@@ -18,10 +18,10 @@ import {
 import {
     Cmp,
     arrayCompare,
-    deepEqual,
     fakeHash,
     fakeUuid,
     keyComparer,
+    deepEqual,
 } from './utils';
 
 //================================================================================ 
@@ -108,6 +108,89 @@ let continueAsyncFollower = async (follower: Follower, storage: IStorage) => {
 
 //================================================================================ 
 // QUERY
+
+type WillMatch = 'all' | 'all-latest' | 'some' | 'nothing';
+interface CleanUpQueryResult {
+    query: Query,
+    isValid: boolean,
+    willMatch: WillMatch;
+}
+let cleanUpQuery = (inputQuery: Query): CleanUpQueryResult => {
+    // check for invalid queries and return null
+    // canonicalize and optimize queries
+    // check for filters that obviously result in nothing and return a canonical empty query: { limit: 0 }
+
+    // apply default values
+    let query = { ...DEFAULT_QUERY, ...inputQuery };
+
+    //--------------------------------------------------
+    // VALIDITY
+
+    let invalidResponse: CleanUpQueryResult = {
+        query: { limit: 0 },
+        isValid: false,
+        willMatch: 'nothing',
+    }
+
+    // TODO: check if contentLength and timestamp are reasonable numbers
+
+    // limit should be a reasonable number
+    if (query.limit !== undefined && query.limit < 0) { return invalidResponse; }
+
+    // if orderBy is path, startAt should contain path or nothing, not localIndex
+    // if orderBy is localIndex, startAt should contain localIndex or nothing, not path
+    if (query.orderBy?.startsWith('path') && query.startAt?.localIndex !== undefined) { return invalidResponse; }
+    if (query.orderBy?.startsWith('localIndex') && query.startAt?.path !== undefined) { return invalidResponse; }
+
+    // valid enum values
+    if (query.history !== undefined && query.history !== 'all' && query.history !== 'latest') { return invalidResponse; }
+    if (query.orderBy !== undefined) {
+        if (['path ASC', 'path DESC', 'localIndex ASC', 'localIndex DESC'].indexOf(query.orderBy) === -1) {
+            return invalidResponse;
+        }
+    }
+
+    //--------------------------------------------------
+    // WILLMATCH
+
+    // start with everything, then narrow down
+    let willMatch: WillMatch = query.history === 'all' ? 'all' : 'all-latest';
+
+    // if there are filters, match some
+    if (query.filter !== undefined && !deepEqual(query.filter, {})) {
+        willMatch = 'some';
+    }
+
+    // a startAt makes us match some
+    if (query.startAt !== undefined && !deepEqual(query.startAt, {})) {
+        willMatch = 'some';
+    }
+
+    // a limit makes us match some or nothing
+    if (query.limit !== undefined) {
+        if (query.limit > 0) { willMatch = 'some'; }
+        if (query.limit === 0) { willMatch = 'nothing'; }
+    }
+
+    // filter combinations that result in no matches --> nothing
+    if (query.filter !== undefined) {
+        let filter = query.filter;
+        if (filter.path && filter.pathStartsWith && !filter.path.startsWith(filter.pathStartsWith)) { willMatch = 'nothing'; }
+        if (filter.path && filter.pathEndsWith && !filter.path.endsWith(filter.pathEndsWith)) { willMatch = 'nothing'; }
+        if (filter.timestamp && filter.timestampGt && !(filter.timestamp > filter.timestampGt)) { willMatch = 'nothing'; }
+        if (filter.timestamp && filter.timestampLt && !(filter.timestamp < filter.timestampLt)) { willMatch = 'nothing'; }
+        if (filter.timestampGt && filter.timestampLt && !(filter.timestampLt+1 < filter.timestampGt)) { willMatch = 'nothing'; }
+        if (filter.contentLength && filter.contentLengthGt && !(filter.contentLength > filter.contentLengthGt)) { willMatch = 'nothing'; }
+        if (filter.contentLength && filter.contentLengthLt && !(filter.contentLength < filter.contentLengthLt)) { willMatch = 'nothing'; }
+        if (filter.contentLengthGt && filter.contentLengthLt && !(filter.contentLengthLt+1 < filter.contentLengthGt)) { willMatch = 'nothing'; }
+    }
+
+    return {
+        query,
+        isValid: true,
+        willMatch,
+    };
+}
 
 let docMatchesFilter = (doc: Doc, filter: QueryFilter): boolean => {
     if (filter.path !== undefined && doc.path !== filter.path) { return false; }
@@ -225,10 +308,12 @@ class Storage implements IStorage {
         return docs[0];
     }
 
-    queryDocs(query?: Query): Doc[] {
+    queryDocs(optionalQuery?: Query): Doc[] {
         // Query the documents.
 
-        query = { ...DEFAULT_QUERY, ...query };
+        // clean up the query and exit early if possible.
+        let { query, isValid, willMatch } = cleanUpQuery(optionalQuery || {});
+        if (!isValid || willMatch === 'nothing' || query.limit === 0) { return []; }
 
         // get history docs or all docs
         let docs = query.history === 'all'
@@ -287,18 +372,23 @@ class Storage implements IStorage {
         return filteredDocs;
     }
 
-    queryPaths(query?: Query): Path[] {
+    queryPaths(optionalQuery?: Query): Path[] {
         // If query is provided:
         // - find docs
         // - get their paths
         // - remove duplicates
         // - sort in ascending order by path unless query has orderBy: 'path DESC'
+        // - limit is applied to the doc query, not the final paths (weird)
         //
         // If query is NOT provided:
         // - return all unique paths in ascending order.
 
+        // clean up the query and exit early if possible.
+        let { query, isValid, willMatch } = cleanUpQuery(optionalQuery || {});
+        if (!isValid || willMatch === 'nothing' || query.limit === 0) { return []; }
+
         let paths: Path[];
-        if (query === undefined || deepEqual(query, {})) {
+        if (willMatch === 'all' || willMatch == 'all-latest') {
             // no query
             // just get list of unique paths
             paths = [...this.docsByPathNewestFirst.keys()];
@@ -316,17 +406,23 @@ class Storage implements IStorage {
         return paths;
     }
 
-    queryAuthors(query?: Query): AuthorAddress[] {
+    queryAuthors(optionalQuery?: Query): AuthorAddress[] {
         // If query is provided:
         // - find docs
         // - get their author addresses
         // - remove duplicates
         // - sort in ascending order by author address
+        // - limit is applied to the doc query, not the final paths (weird)
         //
         // If query is NOT provided:
         // - return all unique author addresses in ascending order.
-        let authors: AuthorAddress[];
-        if (query === undefined || deepEqual(query, {})) {
+
+        // clean up the query and exit early if possible.
+        let { query, isValid, willMatch } = cleanUpQuery(optionalQuery || {});
+        if (!isValid || willMatch === 'nothing' || query.limit === 0) { return []; }
+
+        let authors: Path[];
+        if (willMatch === 'all') {
             // no query
             // just get all unique authors from all docs
             let authorsSet = new Set<AuthorAddress>();
