07a1f122da3b3081df0672183605e0dd5afd5ddc
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-05-19 21:20:12 -0700

    add missing things to entries/universal.ts

--- src/entries/universal.ts
+++ src/entries/universal.ts
@@ -1,25 +1,41 @@
 export * from "../core-validators/addresses";
+export * from "../core-validators/characters";
+export * from "../core-validators/checkers";
 
+export * from "../crypto/base32";
+// TODO: where does chloride go?   it's a browser-only thing
+// skip: crypto-driver-node, it's not universal
+export * from "../crypto/crypto-driver-tweetnacl";
 export * from "../crypto/crypto-types";
 export * from "../crypto/crypto";
+export * from "../crypto/global-crypto-driver";
+export * from "../crypto/keypair";
 
 export * from "../format-validators/format-validator-es4";
 export * from "../format-validators/format-validator-types";
 
-export * from "../query-follower/query-follower-types";
-export * from "../query-follower/query-follower";
+export * from '../peer/peer-client';
+export * from '../peer/peer-server';
+export * from '../peer/peer-types';
+export * from '../peer/peer';
 
 export * from "../query/query-types";
 export * from "../query/query";
 
+export * from "../query-follower/query-follower-types";
+export * from "../query-follower/query-follower";
+
+export * from '../storage/compare';
+export * from '../storage/lock';
 export * from "../storage/storage-async";
 export * from "../storage/storage-cache";
 export * from "../storage/storage-driver-async-memory";
-
 export * from "../storage/storage-types";
+export * from "../storage/util-types";
 
-export * from "../util/doc-types";
-
-export * from "../util/errors";
-
-export * from "../crypto/crypto-driver-tweetnacl";
+export * from '../util/buffers';
+export * from '../util/bytes';
+export * from '../util/doc-types';
+export * from '../util/errors';
+export * from '../util/log';
+export * from '../util/misc';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './core-validators/addresses';
 export * from './core-validators/characters';
 export * from './core-validators/checkers';
+
 export * from './crypto/base32';
 //export * from './crypto/crypto-driver-chloride';
 export * from './crypto/crypto-driver-node';
@@ -9,17 +10,23 @@ export * from './crypto/crypto-types';
 export * from './crypto/crypto';
 export * from './crypto/global-crypto-driver';
 export * from './crypto/keypair';
+
 export * from './format-validators/format-validator-es4';
 export * from './format-validators/format-validator-types';
+
 export * from './peer/peer-client';
 export * from './peer/peer-server';
 export * from './peer/peer-types';
 export * from './peer/peer';
+
 export * from './query/query-types';
 export * from './query/query';
+
 export * from './query-follower/query-follower-types';
 export * from './query-follower/query-follower';
+
 //export * from './shims/node';
+
 export * from './storage/compare';
 export * from './storage/lock';
 export * from './storage/storage-async';
@@ -27,6 +34,7 @@ export * from './storage/storage-cache';
 export * from './storage/storage-driver-async-memory';
 export * from './storage/storage-types';
 export * from './storage/util-types';
+
 export * from './util/buffers';
 export * from './util/bytes';
 export * from './util/doc-types';
