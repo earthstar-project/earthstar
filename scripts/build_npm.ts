d9af2f24a306e0ed142dac92df8ff0a21a10380e
Sam Gwilym <gwilym@me.com>
2022-01-07 19:02:20 +0000

    Add Node v14 support

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -1,52 +1,56 @@
-import { build } from "https://deno.land/x/dnt@0.13.0/mod.ts";
+import { build } from "https://raw.githubusercontent.com/denoland/dnt/968f16ef0ca9ac9379144654f2edb8e92d46a544/mod.ts";
 
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 
 await build({
-    entryPoints: ["./mod.ts", "./src/entries/node.ts"],
-    outDir: "./npm",
-    shims: {
-        deno: {
-            test: "dev",
-        },
-        customDev: [
-            {
-                package: {
-                    name: "@types/chloride",
-                    version: "2.4.0",
-                },
-                globalNames: [],
-            },
-        ],
+  entryPoints: ["./mod.ts", "./src/entries/node.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: {
+      test: "dev",
     },
-    mappings: {
-        "https://deno.land/x/crayon_chalk_aliases@1.1.0/index.ts": {
-            name: "chalk",
-            version: "4.1.2",
+    customDev: [
+      {
+        package: {
+          name: "@types/chloride",
+          version: "2.4.0",
         },
-        "https://cdn.skypack.dev/concurrency-friends@5.2.0?dts": {
-            name: "concurrency-friends",
-            version: "5.2.0",
-        },
-        "./src/node/chloride.ts": {
-            name: "chloride",
-            version: "2.4.1",
-        },
+        globalNames: [],
+      },
+    ],
+  },
+  compilerOptions: {
+    // This is for Node v14 support
+    target: "ES2020",
+  },
+  mappings: {
+    "https://deno.land/x/crayon_chalk_aliases@1.1.0/index.ts": {
+      name: "chalk",
+      version: "4.1.2",
     },
-    package: {
-        // package.json properties
-        name: "stone-soup",
-        version: Deno.args[0],
-        description: "Your package.",
-        license: "MIT",
-        repository: {
-            type: "git",
-            url: "git+https://github.com/username/package.git",
-        },
-        bugs: {
-            url: "https://github.com/username/package/issues",
-        },
+    "https://cdn.skypack.dev/concurrency-friends@5.2.0?dts": {
+      name: "concurrency-friends",
+      version: "5.2.0",
     },
+    "./src/node/chloride.ts": {
+      name: "chloride",
+      version: "2.4.1",
+    },
+  },
+  package: {
+    // package.json properties
+    name: "stone-soup",
+    version: Deno.args[0],
+    description: "Your package.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/username/package.git",
+    },
+    bugs: {
+      url: "https://github.com/username/package/issues",
+    },
+  },
 });
 
 // post build steps
