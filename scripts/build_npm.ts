66a757a8c4ce00bfef2c5f7234f468cadbd88f9b
Sam Gwilym <gwilym@me.com>
2022-01-07 11:11:42 +0000

    Updated Github workflows

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -3,33 +3,33 @@ import { build } from "https://deno.land/x/dnt@0.13.0/mod.ts";
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 
 await build({
-    entryPoints: ["./mod.ts"],
-    outDir: "./npm",
-    shims: {
-        deno: {
-            test: "dev",
-        },
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: {
+      test: "dev",
     },
-    mappings: {
-        "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js": {
-            name: "chalk",
-            version: "4.1.1",
-        },
+  },
+  mappings: {
+    "https://deno.land/x/chalk_deno@v4.1.1-deno/source/index.js": {
+      name: "chalk",
+      version: "4.1.1",
     },
-    package: {
-        // package.json properties
-        name: "stone-soup",
-        version: Deno.args[0],
-        description: "Your package.",
-        license: "MIT",
-        repository: {
-            type: "git",
-            url: "git+https://github.com/username/package.git",
-        },
-        bugs: {
-            url: "https://github.com/username/package/issues",
-        },
+  },
+  package: {
+    // package.json properties
+    name: "stone-soup",
+    version: Deno.args[0],
+    description: "Your package.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/username/package.git",
     },
+    bugs: {
+      url: "https://github.com/username/package/issues",
+    },
+  },
 });
 
 // post build steps
