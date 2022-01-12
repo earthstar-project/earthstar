796eda12d0fa54486e67a7f85d1a8d6acc73e6f4
Sam Gwilym <gwilym@me.com>
2022-01-12 14:35:34 +0100

    Update NPM script with package details

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -3,64 +3,65 @@ import { build } from "https://deno.land/x/dnt@0.15.0/mod.ts";
 await Deno.remove("npm", { recursive: true }).catch((_) => {});
 
 await build({
-  entryPoints: ["./mod.ts", "./src/entries/node.ts"],
-  outDir: "./npm",
-  shims: {
-    deno: {
-      test: "dev",
-    },
-    weakRef: true,
-    customDev: [
-      {
-        package: {
-          name: "@types/chloride",
-          version: "2.4.0",
+    entryPoints: ["./mod.ts", "./src/entries/node.ts"],
+    outDir: "./npm",
+    shims: {
+        deno: {
+            test: "dev",
         },
-        globalNames: [],
-      },
-    ],
-  },
-  compilerOptions: {
-    // This is for Node v14 support
-    target: "ES2020",
-  },
-  mappings: {
-    "https://deno.land/x/crayon_chalk_aliases@1.1.0/index.ts": {
-      name: "chalk",
-      version: "4.1.2",
+        weakRef: true,
+        customDev: [
+            {
+                package: {
+                    name: "@types/chloride",
+                    version: "2.4.0",
+                },
+                globalNames: [],
+            },
+        ],
     },
-    "https://cdn.skypack.dev/concurrency-friends@5.2.0?dts": {
-      name: "concurrency-friends",
-      version: "5.2.0",
+    compilerOptions: {
+        // This is for Node v14 support
+        target: "ES2020",
     },
-    "./src/node/chloride.ts": {
-      name: "chloride",
-      version: "2.4.1",
+    mappings: {
+        "https://deno.land/x/crayon_chalk_aliases@1.1.0/index.ts": {
+            name: "chalk",
+            version: "4.1.2",
+        },
+        "https://cdn.skypack.dev/concurrency-friends@5.2.0?dts": {
+            name: "concurrency-friends",
+            version: "5.2.0",
+        },
+        "./src/node/chloride.ts": {
+            name: "chloride",
+            version: "2.4.1",
+        },
+        "https://raw.githubusercontent.com/sgwilym/noble-ed25519/6e05e960522e6def6974769d5bff032eaed68685/mod.ts":
+            {
+                name: "@noble/ed25519",
+                version: "1.4.0",
+            },
     },
-    "https://raw.githubusercontent.com/sgwilym/noble-ed25519/6e05e960522e6def6974769d5bff032eaed68685/mod.ts": {
-      name: "@noble/ed25519",
-      version: "1.4.0",
+    package: {
+        // package.json properties
+        name: "stone-soup",
+        version: Deno.args[0],
+        description: "A distributed, syncable key-value database",
+        license: "AGPL-3.0",
+        repository: {
+            type: "git",
+            url: "git+https://github.com/earthstar-project/stone-soup.git",
+        },
+        bugs: {
+            url: "https://github.com/earthstar-project/stone-soup/issues",
+        },
     },
-  },
-  package: {
-    // package.json properties
-    name: "stone-soup",
-    version: Deno.args[0],
-    description: "Your package.",
-    license: "MIT",
-    repository: {
-      type: "git",
-      url: "git+https://github.com/username/package.git",
+    // tsc includes 'dom' as a lib, so doesn't need IndexedDB types
+    redirects: {
+        "./src/storage/indexeddb-types.deno.d.ts":
+            "./src/storage/indexeddb-types.node.d.ts",
     },
-    bugs: {
-      url: "https://github.com/username/package/issues",
-    },
-  },
-  // tsc includes 'dom' as a lib, so doesn't need IndexedDB types
-  redirects: {
-    "./src/storage/indexeddb-types.deno.d.ts":
-      "./src/storage/indexeddb-types.node.d.ts",
-  },
 });
 
 // post build steps
