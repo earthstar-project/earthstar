d501410b368eaf4295f15206e0ec7ef666c8d850
Sam Gwilym <gwilym@me.com>
2022-01-24 12:05:25 +0100

    Add description for Peer / IPeer

--- src/peer/peer-types.ts
+++ src/peer/peer-types.ts
@@ -8,21 +8,22 @@ import { Crypto } from "../crypto/crypto.ts";
 
 export type PeerId = string;
 
+/** Holds many shares' replicas and manages their synchronisation with other peers. Recommended as the point of contact between your application and Earthstar shares. */
 export interface IPeer {
-    // TODO: oops, or should we have storage IDs instead of peer IDs?
-    peerId: PeerId;
+  // TODO: oops, or should we have storage IDs instead of peer IDs?
+  peerId: PeerId;
 
-    // getters
-    hasWorkspace(workspace: WorkspaceAddress): boolean;
-    workspaces(): WorkspaceAddress[];
-    storages(): IStorageAsync[];
-    size(): number;
-    getStorage(ws: WorkspaceAddress): IStorageAsync | undefined;
+  // getters
+  hasWorkspace(workspace: WorkspaceAddress): boolean;
+  workspaces(): WorkspaceAddress[];
+  storages(): IStorageAsync[];
+  size(): number;
+  getStorage(ws: WorkspaceAddress): IStorageAsync | undefined;
 
-    // setters
-    addStorage(storage: IStorageAsync): Promise<void>;
-    removeStorageByWorkspace(workspace: WorkspaceAddress): Promise<void>;
-    removeStorage(storage: IStorageAsync): Promise<void>;
+  // setters
+  addStorage(storage: IStorageAsync): Promise<void>;
+  removeStorageByWorkspace(workspace: WorkspaceAddress): Promise<void>;
+  removeStorage(storage: IStorageAsync): Promise<void>;
 }
 
 //================================================================================
@@ -71,8 +72,8 @@ export interface IPeer {
 
 // ok this isn't a type, but I put it here anyway since it's shared code for client and server
 export let saltAndHashWorkspace = async (
-    salt: string,
-    workspace: WorkspaceAddress,
+  salt: string,
+  workspace: WorkspaceAddress,
 ): Promise<string> => await Crypto.sha256base32(salt + workspace + salt);
 
 //--------------------------------------------------
@@ -81,27 +82,27 @@ export let saltAndHashWorkspace = async (
 export interface SaltyHandshake_Request {
 }
 export interface SaltyHandshake_Response {
-    serverPeerId: PeerId;
-    salt: string;
-    saltedWorkspaces: string[];
+  serverPeerId: PeerId;
+  salt: string;
+  saltedWorkspaces: string[];
 }
 
 //--------------------------------------------------
 // ask server for all storage states
 
 export interface AllWorkspaceStates_Request {
-    commonWorkspaces: WorkspaceAddress[];
+  commonWorkspaces: WorkspaceAddress[];
 }
 export type AllWorkspaceStates_Response = {
-    serverPeerId: PeerId;
-    workspaceStatesFromServer: Record<
-        WorkspaceAddress,
-        WorkspaceStateFromServer
-    >;
+  serverPeerId: PeerId;
+  workspaceStatesFromServer: Record<
+    WorkspaceAddress,
+    WorkspaceStateFromServer
+  >;
 };
 export type AllWorkspaceStates_Outcome = Record<
-    WorkspaceAddress,
-    WorkspaceState
+  WorkspaceAddress,
+  WorkspaceState
 >;
 
 //--------------------------------------------------
@@ -109,15 +110,15 @@ export type AllWorkspaceStates_Outcome = Record<
 // this only pulls client<--server, does not push client-->server
 
 export interface WorkspaceQuery_Request {
-    workspace: WorkspaceAddress;
-    storageId: StorageId;
-    query: Query;
+  workspace: WorkspaceAddress;
+  storageId: StorageId;
+  query: Query;
 }
 export interface WorkspaceQuery_Response {
-    workspace: WorkspaceAddress;
-    storageId: StorageId;
-    serverMaxLocalIndexOverall: number;
-    docs: Doc[];
+  workspace: WorkspaceAddress;
+  storageId: StorageId;
+  serverMaxLocalIndexOverall: number;
+  docs: Doc[];
 }
 
 //--------------------------------------------------
@@ -126,86 +127,86 @@ export interface WorkspaceQuery_Response {
 // Null means not known yet.
 // This should be easily serializable.
 export interface PeerClientState {
-    serverPeerId: PeerId | null;
-    // TODO: commonWorkspaces could be merged with storageSyncStates?
-    commonWorkspaces: WorkspaceAddress[] | null;
-    workspaceStates: Record<WorkspaceAddress, WorkspaceState>;
-    lastSeenAt: number | null; // a timestamp in Earthstar-style microseconds
+  serverPeerId: PeerId | null;
+  // TODO: commonWorkspaces could be merged with storageSyncStates?
+  commonWorkspaces: WorkspaceAddress[] | null;
+  workspaceStates: Record<WorkspaceAddress, WorkspaceState>;
+  lastSeenAt: number | null; // a timestamp in Earthstar-style microseconds
 }
 export interface WorkspaceStateFromServer {
-    workspace: WorkspaceAddress;
-    serverStorageId: StorageId;
-    serverMaxLocalIndexOverall: number;
+  workspace: WorkspaceAddress;
+  serverStorageId: StorageId;
+  serverMaxLocalIndexOverall: number;
 }
 export interface WorkspaceState {
-    workspace: WorkspaceAddress;
-    serverStorageId: StorageId;
-    serverMaxLocalIndexOverall: number;
-    serverMaxLocalIndexSoFar: number; // -1 if unknown
-    clientStorageId: StorageId;
-    clientMaxLocalIndexOverall: number;
-    clientMaxLocalIndexSoFar: number; // -1 if unknown
-    lastSeenAt: number;
+  workspace: WorkspaceAddress;
+  serverStorageId: StorageId;
+  serverMaxLocalIndexOverall: number;
+  serverMaxLocalIndexSoFar: number; // -1 if unknown
+  clientStorageId: StorageId;
+  clientMaxLocalIndexOverall: number;
+  clientMaxLocalIndexSoFar: number; // -1 if unknown
+  lastSeenAt: number;
 }
 
 export let initialPeerClientState: PeerClientState = {
-    serverPeerId: null,
-    commonWorkspaces: null,
-    workspaceStates: {},
-    lastSeenAt: null,
+  serverPeerId: null,
+  commonWorkspaces: null,
+  workspaceStates: {},
+  lastSeenAt: null,
 };
 
 export interface IPeerClient {
-    // Each client only talks to one server.
+  // Each client only talks to one server.
 
-    // this is async in case we later want to set up
-    // a message bus that alerts when the state is changed
-    setState(newState: Partial<PeerClientState>): Promise<void>;
+  // this is async in case we later want to set up
+  // a message bus that alerts when the state is changed
+  setState(newState: Partial<PeerClientState>): Promise<void>;
 
-    // get and return the server's peerId.
-    // this is small and simple and it be used as a ping to check if the server is online.
-    do_getServerPeerId(): Promise<PeerId>;
+  // get and return the server's peerId.
+  // this is small and simple and it be used as a ping to check if the server is online.
+  do_getServerPeerId(): Promise<PeerId>;
 
-    // figure out workspaces we have in common
-    // do_: launches the request, runs handle_, and updates our state with the result
-    do_saltyHandshake(): Promise<void>;
-    handle_saltyHandshake(
-        response: SaltyHandshake_Response,
-    ): Promise<Partial<PeerClientState>>;
+  // figure out workspaces we have in common
+  // do_: launches the request, runs handle_, and updates our state with the result
+  do_saltyHandshake(): Promise<void>;
+  handle_saltyHandshake(
+    response: SaltyHandshake_Response,
+  ): Promise<Partial<PeerClientState>>;
 
-    // get workspace states from the server (localIndex numbers)
-    // do_: launches the request, runs handle_, and updates our state with the result
-    do_allWorkspaceStates(): Promise<void>;
-    handle_allWorkspaceStates(
-        request: AllWorkspaceStates_Request,
-        response: AllWorkspaceStates_Response,
-    ): Promise<Partial<PeerClientState>>;
+  // get workspace states from the server (localIndex numbers)
+  // do_: launches the request, runs handle_, and updates our state with the result
+  do_allWorkspaceStates(): Promise<void>;
+  handle_allWorkspaceStates(
+    request: AllWorkspaceStates_Request,
+    response: AllWorkspaceStates_Response,
+  ): Promise<Partial<PeerClientState>>;
 
-    // do a query and ingest the results
-    // do_: launches the request, runs process_, returns number of docs obtained that were not invalid
-    do_workspaceQuery(request: WorkspaceQuery_Request): Promise<number>;
-    process_workspaceQuery(response: WorkspaceQuery_Response): Promise<number>;
+  // do a query and ingest the results
+  // do_: launches the request, runs process_, returns number of docs obtained that were not invalid
+  do_workspaceQuery(request: WorkspaceQuery_Request): Promise<number>;
+  process_workspaceQuery(response: WorkspaceQuery_Response): Promise<number>;
 }
 
 //--------------------------------------------------
 
 export interface IPeerServer {
-    // this does not affect any internal state, in fact
-    // the server has no internal state (except maybe for
-    // rate limiting, etc)
+  // this does not affect any internal state, in fact
+  // the server has no internal state (except maybe for
+  // rate limiting, etc)
 
-    // this class will be exposed over RPC --
-    // make sure it only has methods that are safe to be exposed to the internet.
+  // this class will be exposed over RPC --
+  // make sure it only has methods that are safe to be exposed to the internet.
 
-    serve_peerId(): Promise<PeerId>;
+  serve_peerId(): Promise<PeerId>;
 
-    serve_saltyHandshake(
-        request: SaltyHandshake_Request,
-    ): Promise<SaltyHandshake_Response>;
-    serve_allWorkspaceStates(
-        request: AllWorkspaceStates_Request,
-    ): Promise<AllWorkspaceStates_Response>;
-    serve_workspaceQuery(
-        request: WorkspaceQuery_Request,
-    ): Promise<WorkspaceQuery_Response>;
+  serve_saltyHandshake(
+    request: SaltyHandshake_Request,
+  ): Promise<SaltyHandshake_Response>;
+  serve_allWorkspaceStates(
+    request: AllWorkspaceStates_Request,
+  ): Promise<AllWorkspaceStates_Response>;
+  serve_workspaceQuery(
+    request: WorkspaceQuery_Request,
+  ): Promise<WorkspaceQuery_Response>;
 }
