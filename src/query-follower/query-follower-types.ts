7549706dfa28df737b7b89adf47726014f3bd92c
Sam Gwilym <gwilym@me.com>
2022-01-25 16:08:58 +0100

    Format some extra documents

--- src/query-follower/query-follower-types.ts
+++ src/query-follower/query-follower-types.ts
@@ -13,11 +13,11 @@ import { IStorageAsync, LiveQueryEvent } from "../storage/storage-types.ts";
  * - `error` — an unexpected error happened, maybe in your bus subscription handler.
  */
 export type QueryFollowerState =
-  | "new"
-  | "catching-up"
-  | "live"
-  | "closed"
-  | "error";
+    | "new"
+    | "catching-up"
+    | "live"
+    | "closed"
+    | "error";
 
 /**
  * Subscribe to the ongoing results of a query, optionally including old existing docs.
@@ -33,40 +33,40 @@ export type QueryFollowerState =
  * ```
  */
 export interface IQueryFollower {
-  storage: IStorageAsync;
-  /**
-   * The query being followed. Has some limitations:
-   * - `historyMode` must be `all`
-   * - `orderBy` must be `localIndex ASC`
-   * - limit can NOT be set.
-   */
-  query: Query;
+    storage: IStorageAsync;
+    /**
+     * The query being followed. Has some limitations:
+     * - `historyMode` must be `all`
+     * - `orderBy` must be `localIndex ASC`
+     * - limit can NOT be set.
+     */
+    query: Query;
 
-  /** Use this to subcribe to events with a callback, which will be called blockingly (one event at a time, one callback at a time).
-   *
-   * For now it's tricky to close a query follower from inside its own event handler; you have to do it using setTimeout or you'll deadlock on the bus's lock.
-   * ```ts
-   * qf.bus.on(await (event) => {
-   *   setTimeout(() => qf.close(), 0);
-   * });
-   * ```
-   * (because you can't send an event from inside an event handler)
-   */
-  bus: Simplebus<LiveQueryEvent>;
+    /** Use this to subcribe to events with a callback, which will be called blockingly (one event at a time, one callback at a time).
+     *
+     * For now it's tricky to close a query follower from inside its own event handler; you have to do it using setTimeout or you'll deadlock on the bus's lock.
+     * ```ts
+     * qf.bus.on(await (event) => {
+     *   setTimeout(() => qf.close(), 0);
+     * });
+     * ```
+     * (because you can't send an event from inside an event handler)
+     */
+    bus: Simplebus<LiveQueryEvent>;
 
-  /** Returns the follower's state, which can be in two modes:
-   *   1. catching up with the backlog
-   *   2. caught up; processing new events as they happen.
-   * When the query follower is in catching-up mode, it runs independently on its own schedule.  When it's in live mode, it processes each doc  as it's written, blockingly, which means it provides backpressure all the way back up to whatever is trying to ingest() docs into the storage.
-   * There is not currently an easy way to know when a query follower has caught up and switched to live mode, except to listen for the 'idle' event on its bus.
-   */
-  state(): QueryFollowerState;
+    /** Returns the follower's state, which can be in two modes:
+     *   1. catching up with the backlog
+     *   2. caught up; processing new events as they happen.
+     * When the query follower is in catching-up mode, it runs independently on its own schedule.  When it's in live mode, it processes each doc  as it's written, blockingly, which means it provides backpressure all the way back up to whatever is trying to ingest() docs into the storage.
+     * There is not currently an easy way to know when a query follower has caught up and switched to live mode, except to listen for the 'idle' event on its bus.
+     */
+    state(): QueryFollowerState;
 
-  /** Begins the process of catching up with existing documents (if needed), then switches to live mode. */
-  hatch(): Promise<void>;
+    /** Begins the process of catching up with existing documents (if needed), then switches to live mode. */
+    hatch(): Promise<void>;
 
-  /** Permanently shut down the QueryFollower, unhooking from the storage and stopping the processing of events. Automatically called when the followed storage closes. */
-  // Triggered when the storage closes
-  // Can also be called manually if you just want to destroy this queryFollower.
-  close(): Promise<void>;
+    /** Permanently shut down the QueryFollower, unhooking from the storage and stopping the processing of events. Automatically called when the followed storage closes. */
+    // Triggered when the storage closes
+    // Can also be called manually if you just want to destroy this queryFollower.
+    close(): Promise<void>;
 }
