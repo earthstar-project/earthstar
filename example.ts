0ba8e7b3e79b801b62d1674f13690bf9d41cdfaa
Sam Gwilym <gwilym@me.com>
2022-08-11 14:43:12 +0200

    Update example a little more...

--- example.ts
+++ example.ts
@@ -1,17 +1,18 @@
 import * as Earthstar from "./mod.ts";
 import { assert } from "https://deno.land/std@0.150.0/testing/asserts.ts";
 
+// In which the story begins and we generate some keypairs.
+// =======================================================================
+
 logNarrator("This is a story about two friends.");
 
 logRabbit("I'm Rabbit!");
 logFrog("And I'm Frog.");
 
 logNarrator(
-  `Because they are animals living in the woods, they had intermittent internet connections, and a canny recognition of the limits of human systems.
+  `Because they are animals living in the woods, they had intermittent internet connections, and an aversion to the new wave of high-modernist systems. Hence their interest in Earthstar.
   
-Hence their interest in p2p systems such as Earthstar.
-  
-They started by generating keypairs for themselves:`,
+The first thing they needed were keypairs for themselves:`,
 );
 
 const rabbitKeypair = await Earthstar.Crypto.generateAuthorKeypair("bunn");
@@ -23,7 +24,18 @@ assert(!Earthstar.isErr(frogKeypair));
 logRabbit(`My identity's address is ${rabbitKeypair.address}`);
 logFrog(`And mine's ${frogKeypair.address}`);
 
-logNarrator("And decided on a share address to keep between themselves...");
+logNarrator(
+  `These keypairs would let them sign their data, so that they could know for certain who wrote what.`,
+);
+
+nextPartPrompt();
+
+// In which we define a share address.
+// =======================================================================
+
+logNarrator(
+  `They needed a spot to keep their data, and came up with an address they shared between themselves:`,
+);
 
 const shareAddress = Earthstar.generateShareAddress("wimbleywoods");
 
@@ -33,9 +45,16 @@ logRabbit(`Pssst... our share address is ${shareAddress}`);
 logFrog("Cool, got it.");
 logRabbit(`See you later!`);
 
-logNarrator(`... and went their separate ways.
-  
-At home, they each created their own 'Replica', their own personal copy of all the data kept in ${shareAddress}.`);
+logNarrator(`... and went their separate ways.`);
+
+nextPartPrompt();
+
+// In which we instantiate some replicas.
+// =======================================================================
+
+logNarrator(
+  `When they got home, they each created their own 'Replica', their own personal copy of all the data kept in ${shareAddress}.`,
+);
 
 logRabbit(`Says here this thing needs a 'driver'...`);
 
@@ -44,7 +63,7 @@ const replicaRabbit = new Earthstar.Replica({ driver: driverRabbit });
 
 logReplica(`Greetings, User! I am ${replicaRabbit.replicaId}!`);
 
-logNarrator("Meanwhile...");
+logNarrator("...");
 
 logFrog("I wonder if there are other kinds of driver?");
 
@@ -55,30 +74,57 @@ logReplica(`Greetings, User! I am ${replicaFrog.replicaId}!`);
 
 logFrog("Charmed.");
 
+logNarrator("They had everything they needed.");
+
+nextPartPrompt();
+
+// In which frog writes some data.
+// =======================================================================
+
 logNarrator(
   "Now that they had their own replicas, they could write data to their heart's content. Frog got home first.",
 );
 
 logFrog("Right. Let's break this thing in...");
+logFrog("So I have to choose a path to write my data to?");
 
 const frogsFirstDoc = await replicaFrog.set(frogKeypair, {
   path: "/test",
   text:
-    "I twisted the key. The 300 horsepower engine purred to life as I rested my webbed hand on the gearstick. Without hesitation I let the handbrake loose and roared into the distance. What awaited me over that horizon? Who can say.",
+    "I twisted the key. The 300 horsepower engine purred to life as I rested my webbed hand on the gearstick. Without hesitation I let the handbrake loose and roared into the distance. What awaited me over that horizon? Who can say...",
 });
 
 assert(!Earthstar.isErr(frogsFirstDoc));
 
-logReplica("Document written!");
-logFrog("It's beautiful...");
+logReplica("@frog wrote indulgent prose at /test");
 console.group();
 console.log(frogsFirstDoc);
 console.groupEnd();
 
+logFrog("Now I'm in the flow...");
+
+const frogsSecondDoc = await replicaFrog.set(frogKeypair, {
+  path: "/story_part_2",
+  text:
+    "\"I'm strictly liquor, love, and lies\" said the scaly beauty before me. I'd bitten off more than I could chew, that much was clear. Any other amphibian would have slid away like they tadpole they were. But me? I had plans. I took the plans out of my bag,",
+});
+
+assert(!Earthstar.isErr(frogsSecondDoc));
+
+logReplica("@frog wrote more indulgent prose at /story_part_2");
+console.group();
+console.log(frogsFirstDoc);
+console.groupEnd();
+
+nextPartPrompt();
+
+// In which rabbit writes some data.
+// =======================================================================
+
 logNarrator("And Rabbit returned home later after a leisurely stroll.");
 
 logRabbit(
-  `How does this thing work? Any data I write needs to be at a particular 'path'...`,
+  `Any data I write needs to be at a particular 'path'...`,
 );
 
 const rabbitsFirstDoc = await replicaRabbit.set(rabbitKeypair, {
@@ -88,8 +134,7 @@ const rabbitsFirstDoc = await replicaRabbit.set(rabbitKeypair, {
 
 assert(!Earthstar.isErr(rabbitsFirstDoc));
 
-logReplica("Document written!");
-logRabbit("Let's take a look...");
+logReplica("@bunn wrote some test data at /test");
 console.group();
 console.log(rabbitsFirstDoc);
 console.groupEnd();
@@ -103,16 +148,23 @@ const rabbitsSecondDoc = await replicaRabbit.set(rabbitKeypair, {
 });
 
 assert(!Earthstar.isErr(frogsFirstDoc));
-logReplica("Document written!");
+logReplica("@bunn wrote a recipe at /carrot_soup");
 console.group();
 console.log(rabbitsSecondDoc);
 console.groupEnd();
 
 logNarrator(
-  "... and so on. The next morning they hurried to their favourite meeting spot to show their hard work to each other.",
+  `... did you notice how they both wrote some data at /test?`,
 );
 
-logFrog("I got through my creative block last night! You have to see this...");
+nextPartPrompt();
+
+// In which we instantiate some peers, and sync.
+// =======================================================================
+
+logNarrator(`The next morning they met up again.`);
+
+logFrog("I got through my creative block last night! It's ribbeting stuff...");
 logRabbit(
   "I know you prefer flies, but do give this carrot soup recipe a try sometime.",
 );
@@ -122,9 +174,8 @@ logNarrator(
 );
 
 logRabbit(
-  "I read the manual. We both need to make something called a Peer to sync data.",
+  "First we both need to put our replicas in something called a 'Peer' to sync data.",
 );
-logFrog("One sec...");
 
 const peerRabbit = new Earthstar.Peer();
 const peerFrog = new Earthstar.Peer();
@@ -132,25 +183,174 @@ const peerFrog = new Earthstar.Peer();
 peerRabbit.addReplica(replicaRabbit);
 peerFrog.addReplica(replicaFrog);
 
-logRabbit("Here goes nothing...");
+logPeer("Greetings.");
+logPeer("Howdy y'all!");
+
+logFrog("Why does my one talk like that?");
+logRabbit("Let's sync them.");
 
 const syncer = peerRabbit.sync(peerFrog);
 
 await syncer.isDone;
 
-logReplica("Synced, baby!!!");
+logPeer("Completed sync.");
+logPeer("Yeehaw!");
 
-logFrog("... Why does it talk like that?");
+logFrog("...");
 
 logNarrator(
-  "But then they looked inside their replicas and were somewhat surprised.",
+  "They looked inside their replicas and were somewhat surprised.",
 );
 
-logFrog("Oh, here's your ");
+nextPartPrompt();
+
+// In which we inspect Rabbit's latest docs.
+// =======================================================================
+
+logNarrator("Rabbit was confused.");
+
+logRabbit("I seem to be missing the first half of your story?");
+
+const rabbitLatestDocs = await replicaRabbit.getLatestDocs();
+
+logReplica("Got Rabbit's latest docs.");
+
+console.group();
+console.log(rabbitLatestDocs);
+console.groupEnd();
+
+nextPartPrompt();
+
+// In which we inspect Frog's latest docs.
+// =======================================================================
+
+logNarrator("And Frog was hopping mad.");
+
+logFrog("What happened to the first part of my story?! That's the best bit!");
+
+const frogLatestDocs = await replicaRabbit.getLatestDocs();
+
+logReplica("Got Frog's latest docs.");
+
+console.group();
+console.log(frogLatestDocs);
+console.groupEnd();
+
+logFrog("I don't even like soup!");
+
+logNarrator("But things were working just as they should.");
+
+nextPartPrompt();
+
+// In which we inspect all versions of docs at a path.
+// =======================================================================
+
+logNarrator("And frog's story was far from lost.");
+
+logPeer(`Chin up pardner'. I got yer' story right here.`);
+
+logReplica("Got _all_ documents at /test");
+
+const allDocsAtTest = await replicaFrog.getAllDocsAtPath("/test");
+
+console.group();
+console.log(allDocsAtTest);
+console.groupEnd();
+
+logFrog("There it is! My story!");
+logRabbit("Oh, I have that document too...");
+logPeer(
+  "Thing is, Rabbit's doc was written after Frog's - so Rabbit's /test document is the top dog!",
+);
+logPeer(
+  "But we still keep each author's last version so that no-one can lose their, uh, precious data.",
+);
+logFrog("<crying with joy>");
+
+logRabbit(
+  "There there. Maybe there's a way to have a place only you can write data to?",
+);
+
+logNarrator(
+  "Could frog find a way to protect his story from being overwritten?",
+);
+
+nextPartPrompt();
+
+// In which Frog writes to an owned path
+// =======================================================================
+
+logNarrator("He could.");
+
+logFrog(
+  "Looks like if you include your keypair's address in the path, prefixed by a tilde...",
+);
+
+const frogsOwnedDoc = await replicaFrog.set(frogKeypair, {
+  path: `/~${frogKeypair.address}/stories/pt1`,
+  text: frogsFirstDoc.text,
+});
+
+console.group();
+console.log(frogsOwnedDoc);
+console.groupEnd();
+
+assert(!Earthstar.isErr(frogsOwnedDoc));
+
+logReplica("@frog re-wrote his story to an owned path!");
+logRabbit("We should test this...");
+
+const rabbitsAttempt = await replicaRabbit.set(rabbitKeypair, {
+  path: `/~${frogKeypair.address}/stories/pt1`,
+  text: frogsFirstDoc.text,
+});
+
+assert(Earthstar.isErr(rabbitsAttempt));
+
+logReplica("Error! @bunn tried to write to a path owned by @frog!");
+logFrog("It worked! What other stuff can we do?");
+logPeer("How about auto-destructing documents?");
+logFrog("Yeah!");
+logRabbit("Yeah!");
+
+nextPartPrompt();
+
+logNarrator("To be continued...");
+
+// In which Rabbit writes an ephemeral doc
+// =======================================================================
+
+// In which Frog saves some attachments
+// =======================================================================
+
+// In which Rabbit adds another share to his peer
+// =======================================================================
+
+// In which they start a replica server to sync over the internet?
+// =======================================================================
 
 Deno.exit(0);
 
-// =====================================
+// In which I write a bunch of utility functions
+// =======================================================================
+
+function nextPartPrompt() {
+  const confirmed = confirm(`Continue?`);
+
+  if (confirmed === false) {
+    console.log(`
+And so the story abruptly ended.
+      `);
+    logFrog("Are we still getting paid for this?");
+    Deno.exit(0);
+  } else {
+    console.log(
+      `%c
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`,
+      "color: lightgrey;",
+    );
+  }
+}
 
 function logNarrator(text: string) {
   console.log(`
@@ -168,4 +368,8 @@ function logFrog(text: string) {
 
 function logReplica(text: string) {
   console.log(`%c   📂 ${text}`, "color: grey;");
+}
+
+function logPeer(text: string) {
+  console.log(`%c   🤖 ${text}`, "color: blue;");
 }
