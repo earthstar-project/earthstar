ec94cdf6b43aacc150a58e0b79ea9d860e8ccfb9
Sam Gwilym <gwilym@me.com>
2022-11-17 16:08:08 +0100

    Use published version of range_reconcile

--- deps.ts
+++ deps.ts
@@ -8,11 +8,14 @@ export * as rfc4648 from "https://esm.sh/rfc4648@1.5.0";
 export * as sha256_uint8array from "https://esm.sh/sha256-uint8array@0.10.3";
 export * as ed from "https://raw.githubusercontent.com/sgwilym/noble-ed25519/153f9e7e9952ad22885f5abb3f6abf777bef4a4c/mod.ts";
 export { hash as xxhash64, XXH64 } from "./src/util/xxhash64.js";
-export { FingerprintTree, RangeMessenger } from "../range-reconcile/mod.ts";
+export {
+  FingerprintTree,
+  RangeMessenger,
+} from "https://deno.land/x/range_reconcile@0.1.0/mod.ts";
 export type {
   LiftingMonoid,
   RangeMessengerConfig,
-} from "../range-reconcile/mod.ts";
+} from "https://deno.land/x/range_reconcile@0.1.0/mod.ts";
 export { AsyncQueue } from "https://deno.land/x/for_awaitable_queue@1.0.0/mod.ts";
 
 // Deno std lib
