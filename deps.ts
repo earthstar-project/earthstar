60e4b9614c1930116a57fb5dc4e3bb0d9af53d3d
Sam Gwilym <sam@gwil.garden>
2023-08-31 10:46:57 +0200

    Use npm specifier instead of esm.sh 🤷‍♂️

--- deps.ts
+++ deps.ts
@@ -2,7 +2,7 @@ export {
   shallowEqualArrays,
   shallowEqualObjects,
 } from "https://deno.land/x/shallow_equal@v0.1.3/mod.ts";
-export { default as fast_json_stable_stringify } from "https://esm.sh/fast-json-stable-stringify@2.1.0";
+export { default as fast_json_stable_stringify } from "npm:fast-json-stable-stringify@2.1.0";
 export * as rfc4648 from "https://esm.sh/rfc4648@1.5.0";
 export * as sha256_uint8array from "https://esm.sh/sha256-uint8array@0.10.3";
 export * as ed from "https://raw.githubusercontent.com/sgwilym/noble-ed25519/153f9e7e9952ad22885f5abb3f6abf777bef4a4c/mod.ts";
