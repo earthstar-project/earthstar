29cb504ce9b8014740a64ee77f890fec74577ccf
Sam Gwilym <sam@gwil.garden>
2024-05-21 07:50:28 +0100

    Update beta version again

--- mod.deno.ts
+++ mod.deno.ts
@@ -1,3 +1,8 @@
+/**
+ * Modules for using Earthstar on the Deno runtime, e.g. persisting data to the filesystem.
+ *
+ * @module
+ */
 export { RuntimeDriverDeno } from "./src/runtime/driver_deno.ts";
 export { getStorageDriverFilesystem } from "./src/peer/storage_drivers/filesystem.deno.ts";
 export { Server, type ServerOpts } from "./src/server/server.ts";
